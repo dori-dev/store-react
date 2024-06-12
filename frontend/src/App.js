101b58a56a51e1c8341b6ac9afcdabf5aecb606f
dori-dev <mr.dori.dev@gmail.com>
2024-06-12 17:52:28 +0330

    Add bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <h2 className="btn btn-danger">Hello</h2>
+      </div>
+      hello
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import ReactDOM from "react";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-import "index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
