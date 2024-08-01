de3f5f8696608d8413ac24da1ea5aade6ecae86c
dori-dev <mr.dori.dev@gmail.com>
2024-08-01 16:04:38 +0330

    Fetch home data from api

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,16 @@
-import React from "react";
-import products from "../products";
+import React, { useState, useEffect } from "react";
 import Product from "../components/Product/Product";
+import axios from "axios";
 
 function Home() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const sendRequest = async () => {
+      const response = await axios.get("http://localhost:8000/api/products/");
+      setProducts(response.data);
+    };
+    sendRequest();
+  }, []);
   return (
     <div>
       <h1>محصولات</h1>
@@ -10,7 +18,10 @@ function Home() {
         <div className="row">
           {products.map((item) => {
             return (
-              <div className="col col-12 col-sm-12 col-md-6 col-lg-4" key={item._id}>
+              <div
+                className="col col-12 col-sm-12 col-md-6 col-lg-4"
+                key={item._id}
+              >
                 <Product product={item} />
               </div>
             );
