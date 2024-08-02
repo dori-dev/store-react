0142d009e8378077b0e84a2b7fc4f445fb8308ee
dori-dev <mr.dori.dev@gmail.com>
2024-08-02 14:52:33 +0330

    Add provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
 import App from "./App";
+import store from "./store";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
