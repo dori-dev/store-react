f4ca59dd7701e18cce4032715df13eac942e05c4
dori-dev <mr.dori.dev@gmail.com>
2024-08-02 15:42:51 +0330

    Use redux in product detail to display

--- frontend/src/pages/Product.js
+++ frontend/src/pages/Product.js
@@ -1,49 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 
-import axios from "axios";
+import { productDetailAction } from "../action/productAction";
 
 const Product = () => {
   const { id } = useParams();
-
-  const [product, setProduct] = useState({});
-
+  const dispatch = useDispatch();
+  const { loading, product } = useSelector((state) => state.productDetail);
   useEffect(() => {
-    const sendRequest = async () => {
-      const response = await axios.get(
-        `http://localhost:8000/api/products/${id}`
-      );
-      setProduct(response.data);
-    };
-    sendRequest();
-  }, [id]);
+    dispatch(productDetailAction(id));
+  }, [id, dispatch]);
 
   return (
     <div>
       <Link to="/" className="btn btn-light my-3">
         بازگشت به صفحه اصلی
       </Link>
-      <div className="row align-items-start">
-        <div className="col-4">
-          <img className="img-fluid" src={product.image} alt={product.name} />
+      {loading ? (
+        <h2>درحال دریافت محصول...</h2>
+      ) : (
+        <div className="row align-items-start">
+          <div className="col-4">
+            <img className="img-fluid" src={product.image} alt={product.name} />
+          </div>
+          <div className="col-4">
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <h3>{product.name}</h3>
+              </li>
+              <li className="list-group-item">{product.price}</li>
+              <li className="list-group-item">{product.description}</li>
+              <li className="list-group-item"></li>
+            </ul>
+          </div>
+          <div className="col-1"></div>
+          <div className="col-3">
+            <button type="button" className="btn btn-primary btn-block">
+              افزودن به سبد خرید
+            </button>
+          </div>
         </div>
-        <div className="col-4">
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <h3>{product.name}</h3>
-            </li>
-            <li className="list-group-item">{product.price}</li>
-            <li className="list-group-item">{product.description}</li>
-            <li className="list-group-item"></li>
-          </ul>
-        </div>
-        <div className="col-1"></div>
-        <div className="col-3">
-          <button type="button" className="btn btn-primary btn-block">
-            افزودن به سبد خرید
-          </button>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
