dafc60e701dd831d2c96803131539f60cdaf238f
dori-dev <mr.dori.dev@gmail.com>
2024-08-07 19:54:36 +0330

    Design cart page

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -16,11 +16,48 @@ const Cart = () => {
     }
   }, [id, dispatch]);
 
-  console.log(cartItems);
+  const removeFromCartHandler = (id) => {
+    console.log(id);
+  };
 
   return (
-    <div>
-      <h1>سبد خرید </h1>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-8">
+          <h2>سبد خرید</h2>
+          {cartItems.length === 0 ? (
+            <p>سبد خرید خالی است</p>
+          ) : (
+            <ul className="list-group list-group-flush">
+              {cartItems.map((item) => (
+                <li className="list-group-item" key={item.product}>
+                  <div className="row">
+                    <div className="col-2">
+                      <img
+                        className="img-fluid"
+                        src={item.image}
+                        alt={item.name}
+                      />
+                    </div>
+                    <div className="col-3 align-self-center">{item.name}</div>
+                    <div className="col-2 align-self-center">{item.price}</div>
+                    <div className="col-2 align-self-center">
+                      <button
+                        onClick={() => removeFromCartHandler(item.product)}
+                        type="button"
+                        className="btn btn-danger"
+                      >
+                        <i className="fa fa-trash fs-5"></i>
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div className="col-4">One of two columns</div>
+      </div>
     </div>
   );
 };
