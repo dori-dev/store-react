dec1e6542db84475ed7b95e73079ee937e332cc4
dori-dev <mr.dori.dev@gmail.com>
2024-08-07 19:28:39 +0330

    Display cart

--- frontend/src/action/cartAction.js
+++ frontend/src/action/cartAction.js
@@ -1,15 +1,24 @@
 import axios from "axios";
 
 export const addToCart = (id) => async (dispatch, getState) => {
-  const { data } = await axios.get(`http://localhost:8000/api/products/${id}`);
-  dispatch({
-    type: "CART_ADD_ITEM",
-    payload: {
-      product: data._id,
-      name: data.name,
-      image: data.image,
-      price: data.price,
-    },
-  });
-  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+  try {
+    const { data } = await axios.get(
+      `http://localhost:8000/api/products/${id}`
+    );
+    dispatch({
+      type: "CART_ADD_ITEM",
+      payload: {
+        product: data._id,
+        name: data.name,
+        image: data.image,
+        price: data.price,
+      },
+    });
+    localStorage.setItem(
+      "cartItems",
+      JSON.stringify(getState().cart.cartItems)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+import { addToCart } from "../action/cartAction";
 
 const Cart = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  const { cartItems } = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id));
+    }
+  }, [id, dispatch]);
+
+  console.log(cartItems);
+
   return (
     <div>
-سبد خرید      
+      <h1>سبد خرید </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
